dfd2acd7f3ab7f549f55c355015ea9e816d211cd
Tyler Smith <eliasbloodmoon@gmail.com>
2023-02-08 13:49:17 -0600

    Edited BasicView to be a table for a single day.
    Edited index to just show BasicView instead of Sign In

--- src/BasicView.js
+++ src/BasicView.js
@@ -7,16 +7,12 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, date, shiftIN, FirstMO, FirstMI, SecondMO, SecondMI, shiftOUT) {
+  return { name, date, shiftIN, FirstMO, FirstMI, SecondMO, SecondMI, shiftOUT };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Adam Tester','Feb 24, 2023', 1000, 1200, 1300, 1500, 1600, 1800),
 ];
 
 export default function BasicTable() {
@@ -25,11 +21,14 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Employee Name</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Shift Time In</TableCell>
+            <TableCell align="right">1st Meal Time Out</TableCell>
+            <TableCell align="right">1st Meal Time In</TableCell>
+            <TableCell align="right">2nd Meal Time Out</TableCell>
+            <TableCell align="right">2nd Meal Time In</TableCell>
+            <TableCell align="right">Shift Time Out</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -41,10 +40,13 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.date}</TableCell>
+              <TableCell align="right">{row.shiftIN}</TableCell>
+              <TableCell align="right">{row.FirstMO}</TableCell>
+              <TableCell align="right">{row.FirstMI}</TableCell>
+              <TableCell align="right">{row.SecondMO}</TableCell>
+              <TableCell align="right">{row.SecondMI}</TableCell>
+              <TableCell align="right">{row.shiftOUT}</TableCell>
             </TableRow>
           ))}
         </TableBody>
