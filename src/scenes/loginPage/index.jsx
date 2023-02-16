cb412d1f382c3bd8b2abb37429122d5ef3fa1e65
Chao <chernchao@hotmail.com>
2023-02-16 12:46:47 -0600

    Chao Commit

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
         textAlign="center"
         
       >
-        <Typography fontWeight="bold" fontSize="32px" color="primary">
+        <Typography fontWeight="bold" fontSize="32px" color="red">
           Digital Dream Forge
         </Typography>
       </Box>
