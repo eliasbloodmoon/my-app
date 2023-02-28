c3f8f5ddca91354de0c2aa6cd9088f65764ff402
Tyler Smith <eliasbloodmoon@gmail.com>
2023-02-28 14:15:48 -0600

    Use of old code meant deleted things came back

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage/index";
 import LoginPage from "./scenes/loginPage/index";
-import ProfilePage from "./scenes/profilePage/index";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -26,10 +25,6 @@ function App() {
             <Route
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/profile/:userId"
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
             />
             <Route
               path="/admin"
