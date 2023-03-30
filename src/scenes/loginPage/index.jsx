c6e49d03e028257900a087173fd7de38cbe57f15
alancharlesmitchell <alancharlesmitchell@gmail.com>
2023-03-30 16:53:24 -0500

    Rebuilt and updated Version Number

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -19,7 +19,7 @@ const LoginPage = () => {
           Digital Dream Forge
         </Typography>
         <Typography>
-          Version 3.29.2114
+          Version 3.30.1652
         </Typography>
       </Box>
 
